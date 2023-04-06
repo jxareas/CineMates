a2364dace0cb94fc64b384d9892d6f4ab59c9982
Jon Areas <areasjx@gmail.com>
2023-04-06 02:10:40 -0600

    feat(src): populate genres dropdown with api data

--- src/app/home/common/header/header.component.ts
+++ src/app/home/common/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MediaService} from "../../../media/api/media.service";
+import {GenreDto} from "../../../media/dto/get-genres-response";
 
 @Component({
   selector: 'jx-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  genres : GenreDto[];
+
+  constructor(private mediaService : MediaService) {
+
+  }
+
+  ngOnInit(): void {
+    this.mediaService.fetchGenres().subscribe({
+      next: genres => (this.genres = genres),
+    });
+  }
 
 }
--- src/app/home/common/home-common.module.ts
+++ src/app/home/common/home-common.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import {MediaModule} from "../../media/media.module";
+import { MediaModule } from '../../media/media.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule, NgOptimizedImage, MediaModule],
+  imports: [CommonModule, NgOptimizedImage, MediaModule, RouterModule],
   exports: [FooterComponent, HeaderComponent],
 })
 export class HomeCommonModule {}
